0d22fa2cdd99a117d0e5fd27736f0601a93aa191
tumiduong <duong.julie@live.com>
2019-10-20 23:31:06 +0000

    implement user authentication: login/logout, registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
